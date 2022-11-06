c64242d055407356fbc818716fded063ac6c93db
Éric Ivan João António <ericantoniodev@gmail.com>
2022-11-06 17:25:44 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from './screens/login';
+import home from './screens/home';
 
 
 
@@ -11,7 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={login} />
+        <Stack.Screen name="Home" component={home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
